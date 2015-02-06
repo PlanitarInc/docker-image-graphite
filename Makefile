8276e3a5b5a67829aba48148abb27995cb132e34
korya <korya@github.com>
2015-02-06 17:02:37 -0500

    `make test` can be run in b2d environment; move test code to `test.sh`

--- Makefile
+++ Makefile
@@ -17,35 +17,4 @@ clean:
 	docker rmi -f ${IMAGE_NAME} || true
 
 test:
-	@# XXX I'm sorry, mama...
-	@echo Booting the test docker container...
-	@docker run ${IMAGE_NAME} /bin/true
-	$(eval DOCKERID := $(shell docker run -dP ${IMAGE_NAME}))
-	$(eval CARBON_PORT := $(shell docker port ${DOCKERID} 2003 | sed 's/^.*://'))
-	$(eval RENDER_PORT := $(shell docker port ${DOCKERID} 81 | sed 's/^.*://'))
-	$(eval GRAFANA_PORT := $(shell docker port ${DOCKERID} 80 | sed 's/^.*://'))
-	@sleep 3s
-	@echo Running the test...
-	@# Carbon cache overrides all received values in the interval with the
-	@# last recieved value. In our case the interval 1s. Hence wait -- 1s.
-	for i in 1 2 3 4 5; do \
-	  echo "test.count 1 `date +%s`" | nc localhost ${CARBON_PORT}; \
-	  sleep 1s; \
-	done
-	sleep 1s;
-	if !  curl -s 'http://localhost:${RENDER_PORT}/render?target=test.count&format=raw&from=-10seconds' | \
-	  grep -q '^test.count,.*|.*1\.0,1\.0,1\.0,1\.0,1\.0'; then \
-	    echo 'Test failed...'; \
-	    curl -s 'http://localhost:${RENDER_PORT}/render?target=test.count&format=raw&from=-20seconds'; \
-	    docker rm -f ${DOCKERID}; \
-	    false; \
-	fi
-	if !  curl -s 'http://localhost:${GRAFANA_PORT}/' | \
-	  grep -q '<title>Grafana</title>'; then \
-	    echo 'Test failed...'; \
-	    curl -s 'http://localhost:${GRAFANA_PORT}/'; \
-	    docker rm -f ${DOCKERID}; \
-	    false; \
-	fi
-	@docker rm -f ${DOCKERID} >/dev/null
-	@echo 'Test passed'
+#          ./test.sh ${IMAGE_NAME}
---
+++ test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -ex
+
+IMAGE_NAME=$1
+
+curl() {
+  docker run --rm -ti --net host planitar/base curl "$@"
+}
+nc_send() {
+  local line
+  read line
+  docker run --rm -ti --net host planitar/base bash -c "echo $line | nc $1 $2"
+}
+
+echo Booting the test docker container...
+
+docker run --rm ${IMAGE_NAME} /bin/true
+
+DOCKERID=$(docker run -dP ${IMAGE_NAME})
+CARBON_PORT=$(docker port ${DOCKERID} 2003 | sed 's/^.*://')
+RENDER_PORT=$(docker port ${DOCKERID} 81 | sed 's/^.*://')
+GRAFANA_PORT=$(docker port ${DOCKERID} 80 | sed 's/^.*://')
+
+echo Running the test...
+
+# Carbon cache overrides all received values in the interval with the
+# last recieved value. In our case the interval 1s. Hence wait -- 1s.
+for i in 1 2 3 4 5; do \
+  echo "test.count 1 `date +%s`" | nc_send localhost ${CARBON_PORT}; \
+  sleep 1s; \
+done
+
+sleep 1s;
+if ! curl -s "http://localhost:${RENDER_PORT}/render?target=test.count&format=raw&from=-10seconds" | \
+  grep -q '^test.count,.*|.*1\.0,1\.0,1\.0,1\.0,1\.0'; then \
+    echo 'Test failed...'; \
+    curl -s "http://localhost:${RENDER_PORT}/render?target=test.count&format=raw&from=-20seconds"; \
+    docker rm -f ${DOCKERID}; \
+    false; \
+fi
+
+if !  curl -s "http://localhost:${GRAFANA_PORT}/" | \
+  grep -q '<title>Grafana</title>'; then \
+    echo 'Test failed...'; \
+    curl -s "http://localhost:${GRAFANA_PORT}/"; \
+    docker rm -f ${DOCKERID}; \
+    false; \
+fi
+
+docker rm -f ${DOCKERID} >/dev/null
+echo 'Test passed'
